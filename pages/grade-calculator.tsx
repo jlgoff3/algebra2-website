aa9664f08fdbbfbc2b93f2ccf6c04f2db3f5ed72
Jay Goff <jameslgoff3@gmail.com>
2023-06-01 13:55:18 -0400

    Update grade-calculator.tsx

--- pages/grade-calculator.tsx
+++ pages/grade-calculator.tsx
@@ -1,6 +1,10 @@
 import { useState } from 'react'
 
+const STANDARD_PERCENT = 0.095
+const CITIZENSHIP_PERCENT = 0.05
+
 const SBGtoPercent = {
+  'N/A': null,
   '0': 0,
   '0.5': 0.25,
   '1': 0.55,
@@ -10,40 +14,43 @@ const SBGtoPercent = {
 }
 
 const defaultGrades = {
-  'Standard 1': '0',
-  'Standard 2': '0',
-  'Standard 3': '0',
-  'Standard 4': '0',
-  'Standard 5': '0',
-  'Standard 6': '0',
-  'Standard 7': '0',
-  'Standard 8': '0',
-  'Standard 9': '0',
-  'Standard 10': '0',
-  Citizenship: '0',
+  'Standard 1': 'N/A',
+  'Standard 2': 'N/A',
+  'Standard 3': 'N/A',
+  'Standard 4': 'N/A',
+  'Standard 5': 'N/A',
+  'Standard 6': 'N/A',
+  'Standard 7': 'N/A',
+  'Standard 8': 'N/A',
+  'Standard 9': 'N/A',
+  'Standard 10': 'N/A',
 }
 
-const calculateGrades = (rawGrades: typeof defaultGrades) => {
-  const grades = Object.entries(rawGrades)
-  const [_, citizenship] = grades.pop()
-  return (
-    0.095 * grades.reduce((acc, [_, grade]) => acc + SBGtoPercent[grade], 0) +
-    0.05 * SBGtoPercent[citizenship]
-  )
+const calculateGrades = (rawGrades: typeof defaultGrades, citizenship) => {
+  const grades = Object.entries(rawGrades).filter((v) => v !== null)
+  const partial =
+    STANDARD_PERCENT * grades.reduce((acc, [_, grade]) => acc + SBGtoPercent[grade], 0) +
+    CITIZENSHIP_PERCENT * citizenship
+  const correction_factor = STANDARD_PERCENT * grades.length + CITIZENSHIP_PERCENT
+  const finalGrade = partial * correction_factor
+  console.log(finalGrade)
+  return finalGrade
 }
 
 const formatGrade = (rawGrade: number) =>
   Number(rawGrade).toLocaleString(undefined, { style: 'percent', minimumFractionDigits: 2 })
 
 const finalGradeSBG = (finalGrade: number) => {
-  const sortedSBG = Object.entries(SBGtoPercent).sort(([_, a], [__, b]) => a - b)
-  return sortedSBG.find(([_, percent]) => finalGrade <= percent)[0]
+  const sortedSBG = Object.entries(SBGtoPercent)
+    .filter(([_, v]) => v !== null)
+    .sort(([_, a], [__, b]) => a - b)
+  return 1 /* sortedSBG.find(([_, percent]) => finalGrade <= percent)[0] */
 }
 
 const GradeCalculator = () => {
   const [grades, setGrades] = useState(defaultGrades)
   const updateGrade = (e, key) => setGrades({ ...grades, ...{ [key]: e.target.value } })
-  const finalGrade = calculateGrades(grades)
+  const finalGrade = calculateGrades(grades, 100)
   const formattedFinalGrade = formatGrade(finalGrade)
   const finalSBG = finalGradeSBG(finalGrade)
   const passing = finalGrade >= 0.6
@@ -51,7 +58,7 @@ const GradeCalculator = () => {
     <div>
       <h1>Final Grade Calculator</h1>
       <p>
-        Use the dropdowns below to adjust the grades for your standards and citizenship category.
+        Use the dropdowns below to adjust the grades for your standards and citizenship category. Please note that this calculator is not intended to replace your gradebook or communicating with your teacher. Due to certain factors, the grade calculator below may be around 1% off from your actual final grade, and as such, the calculator should only be used as a tool to help you understand what you need to do to achieve the grade level you desire.
         Based on what you've currently entered, your Expected Final Grade is:
         <br />
         <h5 className="text-lg">
@@ -81,6 +88,18 @@ const GradeCalculator = () => {
           </label>
         </div>
       ))}
+      <div className="my-2">
+        <input id="citizenship-points" />
+        <label htmlFor="citizenship-points" className="ml-5">
+          Citizenship Points
+        </label>
+      </div>
+      <div className="my-2">
+        <input id="citizenship-total" />
+        <label htmlFor="citizenship-total" className="ml-5">
+          Total Points
+        </label>
+      </div>
     </div>
   )
 }
