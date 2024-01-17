969b147000fee432d4b25934cf3615ff101f3522
Jay Goff <jameslgoff3@gmail.com>
2024-01-17 10:56:43 -0500

    feat: MTH308A and math pledge

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -8,7 +8,7 @@ const headerNavLinks = [
     children: [
       { href: '/grade-calculator', title: 'Grade Calculator' },
       { href: '/choice-garden', title: 'Garden of Choice' },
-      /* { href: '/mth308a', title: 'Algebra 2A' }, */
+      { href: '/mth308a', title: 'Algebra 2A' },
       { href: '/cutoff-dates', title: 'Cutoff Dates' },
       { href: '/tutoring', title: 'Tutoring Dates' },
       { href: '/birthday', title: 'Birthday Song' },
